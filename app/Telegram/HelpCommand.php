c02c59415ba687ee2068a70fe8a3992f01eadf14
sidharth <sidharth@startupmission.in>
2021-05-06 19:35:57 +0530

    disclaimer

--- app/Telegram/HelpCommand.php
+++ app/Telegram/HelpCommand.php
@@ -36,6 +36,7 @@ class HelpCommand extends Command
         $text .= PHP_EOL;
         $text .= 'Disclaimer: This bot utilises [Co-WIN Public APIs](https://apisetu.gov.in/public/marketplace/api/cowin/cowin-public-v2#/) to provide the information. All information provided by this bot is derived directly from the Co-WIN Public APIs.';
         $text .= PHP_EOL;
+        $text .= PHP_EOL;
         $text .= "Bot developed by [Sidharth Menon](https://in.linkedin.com/in/isidharth)";
 
         $this->replyWithMessage(['text' => $text, 'parse_mode' => 'Markdown']);
